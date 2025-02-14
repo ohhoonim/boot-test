aafeed4cab50b3b227180873fd9b0ebbd2d27d60
mathew <donnj95@gmail.com>
2025-02-15 07:59:34 +0900

    주석보강

--- src/test/java/com/ohhoonim/boot_test/testingLibraries/JsonPathTest.java
+++ src/test/java/com/ohhoonim/boot_test/testingLibraries/JsonPathTest.java
@@ -13,12 +13,17 @@ import com.jayway.jsonpath.JsonPath;
 
 public class JsonPathTest {
     
+    // resource 폴더에서 파일 읽기
     @Test
     public void loadJsonTest() {
         var postsStream = new ClassPathResource("posts.json");
         assertThat(postsStream.getFilename()).isEqualTo("posts.json");
     }
 
+    // sample 데이터 준비하기 
+    // json 문자열을 파싱하기
+    // posts.json - https://jsonplaceholder.typicode.com/posts
+    // users.json - https://jsonplaceholder.typicode.com/users/1
     @Test
     public void parseJsonTest() throws IOException {
         var postsStream = new ClassPathResource("posts.json");
@@ -28,6 +33,7 @@ public class JsonPathTest {
         assertThat(posts.size()).isEqualTo(100); 
     }
 
+    // posts.json을 위한 record
     private record Post(
         Integer userId,
         Integer id,
@@ -39,6 +45,7 @@ public class JsonPathTest {
     // posts.json, user.json 의 결과가 restapi를 호출한 결과라고 가정해보자. 
     // 작성된 코드보다는 JsonPath의 필터 문법에 집중하자 
 
+    // filter를 이용한 리스트 테스트
     @Test
     @DisplayName("특정 user의 포스트가 있는지 확인")
     public void userIdFilterTest() throws IOException {
@@ -49,6 +56,7 @@ public class JsonPathTest {
         assertThat(postsByNine.size()).isLessThan(11);
     }
 
+    // property가 존재하는지 체크
    @Test
    public void hasPropertyTest() throws IOException {
         var postsStream = new ClassPathResource("users.json");
